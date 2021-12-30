69e5ebbde6649ce74c1a49df715f1c8623368bef
Gabriel Viana <g.viana87@hotmail.com>
2021-12-30 19:55:53 -0300

    req 5 - add if else para atribuir situaçao a partir do calculo imc

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-script/imc.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-script/imc.js
@@ -4,12 +4,34 @@ const peso = readline.questionFloat('Qual seu peso: ');
 const altura = readline.questionFloat('Qual sua altura: ');
 
 const calcIMC = (peso, altura) => {
-  const calc = (peso / Math.pow(altura, 2)).toFixed(2);
+  const imc = (peso / Math.pow(altura, 2)).toFixed(2);
 
   console.log('=====================================================');
   console.log(`Considerando peso de ${peso}Kg e alura de ${altura}m:`);
-  console.log(`               O IMC é ${calc}`);
-  console.log('=====================================================');
+  console.log(`               O IMC é ${imc}`);
+  
+  if ( imc < 18.5 ) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
+  }
+  if ( imc >= 18.5 && imc < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+  if ( imc >= 25 && imc < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+  if ( imc >= 30 && imc < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+  if ( imc >= 35 && imc < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }
+
+  console.log('Situação: Obesidade grau III e IV');
 
 };
 
